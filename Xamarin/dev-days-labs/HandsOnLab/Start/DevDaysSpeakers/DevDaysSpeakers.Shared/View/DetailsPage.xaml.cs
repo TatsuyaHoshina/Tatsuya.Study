7dddb6a45107ca73bc3121fc603f00466ac65f1d
hosshy84 <tatsuya.kaiden@gmail.com>
2017-02-26 16:08:51 +0900

    add xamarin

---
+++ Xamarin/dev-days-labs/HandsOnLab/Start/DevDaysSpeakers/DevDaysSpeakers.Shared/View/DetailsPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+using DevDaysSpeakers.Model;
+using Plugin.TextToSpeech;
+
+using DevDaysSpeakers.ViewModel;
+
+namespace DevDaysSpeakers.View
+{
+    public partial class DetailsPage : ContentPage
+    {
+        Speaker speaker;
+		SpeakersViewModel viewModel;
+		public DetailsPage()
+		{
+			InitializeComponent();
+		}
+		public DetailsPage(Speaker speaker, SpeakersViewModel viewModel) : this()
+        {
+            //Set local instance of speaker and set BindingContext
+            this.speaker = speaker;
+			this.viewModel = viewModel;
+            BindingContext = this.speaker;
+
+			ButtonSpeak.Clicked += ButtonSpeak_Clicked;
+			ButtonWebsite.Clicked += ButtonWebsite_Clicked;
+			ButtonSave.Clicked += ButtonSave_Clicked;
+        }
+
+		void ButtonSpeak_Clicked(object sender, EventArgs e)
+		{
+			CrossTextToSpeech.Current.Speak(this.speaker.Description);
+		}
+
+		void ButtonWebsite_Clicked(object sender, EventArgs e)
+		{
+			if (speaker.Website.StartsWith("http"))
+				Device.OpenUri(new Uri(speaker.Website));
+		}
+
+		async void ButtonSave_Clicked(object sender, EventArgs e)
+		{
+			speaker.Title = EntryTitle.Text;
+			await viewModel.UpdateSpeaker(speaker);
+			await Navigation.PopAsync();
+		}
+    }
+}
---
+++ Xamarin/dev-days-labs/HandsOnLab/Start/DevDaysSpeakers/DevDaysSpeakers.Shared/ViewModel/SpeakersViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using DevDaysSpeakers.Model;
+using DevDaysSpeakers.Services;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+
+namespace DevDaysSpeakers.ViewModel
+{
+	public class SpeakersViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		public ObservableCollection<Speaker> Speakers { get; set; }
+		public Command GetSpeakersCommand { get; set; }
+
+		public SpeakersViewModel()
+		{
+			Speakers = new ObservableCollection<Speaker>();
+			GetSpeakersCommand = new Command(async () => await GetSpeakers(), () => !IsBusy);
+		}
+
+		void OnPropertyChanged([CallerMemberName] string name = null) =>
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+		bool busy;
+		public bool IsBusy
+		{
+			get { return busy; }
+			set
+			{
+				busy = value;
+				OnPropertyChanged();
+				GetSpeakersCommand.ChangeCanExecute();
+			}
+		}
+
+		async Task GetSpeakers()
+		{
+			if (IsBusy) return;
+
+			Exception error = null;
+			try
+			{
+				IsBusy = true;
+
+				//using (var client = new HttpClient())
+				//{
+				//	var json = await client.GetStringAsync("http://demo4404797.mockable.io/speakers");
+				//	var items = JsonConvert.DeserializeObject<List<Speaker>>(json);
+
+				//	Speakers.Clear();
+				//	foreach (var item in items)
+				//	{
+				//		Speakers.Add(item);
+				//	}
+				//}
+				var service = DependencyService.Get<AzureService>();
+				var items = await service.GetSpeakers();
+
+				Speakers.Clear();
+				foreach (var item in items)
+				{
+					Speakers.Add(item);
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine($"Error: {ex}");
+				error = ex;
+			}
+			finally
+			{
+				IsBusy = false;
+			}
+
+			if (error != null) await Application.Current.MainPage.DisplayAlert("error!", error.Message, "OK");
+		}
+
+		public async Task UpdateSpeaker(Speaker speaker)
+		{
+			var service = DependencyService.Get<AzureService>();
+			await service.UpdateSpeaker(speaker);
+			await GetSpeakers();
+		}
+	}
+}
